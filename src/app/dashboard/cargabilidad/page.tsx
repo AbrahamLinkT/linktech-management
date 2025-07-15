c588aff5f152fe1f2b48585f19a82be09a769d45
NoeG-J <noe.garces@linktech.com.mx>
2025-07-15 16:43:33 -0600

    modificaciones en contenedor principal, se sigue trbajando en ello, subo cambios para trabajar en equipo personal

--- src/app/dashboard/cargabilidad/page.tsx
+++ src/app/dashboard/cargabilidad/page.tsx
@@ -7,93 +7,95 @@ import { DialogBase } from "../../../components/modal/modals";
 import { Table_1 } from "../../../components/tables/table";
 import { ContentBody, ContentTable } from "../../../components/containers/containers";
 import { LogicaSwtich } from "../../../components/modal/logica";
+import Calendario from "@/components/modal/calendario";
 
 export default function Cargabilidad() {
-    /*
-    const handleclick = () => {
-        console.log("Hola mudno")
-    }
-    return (
-        <>
-            <h2>hola mundo</h2>
-            <button onClick={handleclick} className="border-2 text-2xl bg-red-400"> hoa </button>
-            <button onClick={handleclick}> hoa </button>
-        </>
-    )*/
 
-// referencia al elemento del dialogo 
+    // referencia al elemento del dialogo 
     const dialogRef = useRef<HTMLDialogElement>(null);
     const [menuOpenId, setMenuOpenId] = useState<string | null>(null);
     const [selectedAction, setSelectedAction] = useState<string | null>(null);
     const [selectId, setSelectId] = useState<string | null>(null)
     // abrur y cerrar modal 
     const handleClick = () => {
-    dialogRef.current?.showModal();
+        dialogRef.current?.showModal();
     }
     const handleClose = () => {
-    dialogRef.current?.close();
+        dialogRef.current?.close();
     }
 
     // mostrar el menu de opciones y capturar el valor 
     const toggleMenu = (id: string) => {
-    setMenuOpenId(prev => (prev === id ? null : id));
+        setMenuOpenId(prev => (prev === id ? null : id));
     };
 
     const handleOption = (action: string, id: string) => {
-    console.log(`Acción: ${action}, ID: ${id}`);
-    setSelectedAction(action);       // guardar opción
-    setMenuOpenId(null);             // cerrar menú
-    setSelectId(id)
-    handleClick();                   // abrir modal
+        console.log(`Acción: ${action}, ID: ${id}`);
+        setSelectedAction(action);       // guardar opción
+        setMenuOpenId(null);             // cerrar menú
+        setSelectId(id)
+        handleClick();                   // abrir modal
     };
     return (
-    <>
-        <ContentBody
-        title="Cargabilidad">
-        <ContentTable
-            header={<SearchWorkers />}
-            Body={
-            <Table_1
-                headers={["Consultor", "Especialidad", "Departamento", "Esquema", "Tiempo", "Estatus", ""]}
-                rows={staf.staff.map((p) => [p.consultor, p.especialidad, p.departamento, p.esquema, p.tiempo, p.estatus,
-                <>
-                <Btn_data text="..." Onclick={() => toggleMenu(p.id)} />
-                {menuOpenId === p.id && (
+        <>
 
-                    <Btn_list
-                    items={[
-                        {
-                        text: "Horas",
-                        onClick: () => handleOption("horas", p.id),
-                        },
-                        {
-                        text: "Proyectos",
-                        onClick: () => handleOption("proyectos", p.id),
-                        },
-                    ]}
-                    />
+            <ContentBody
+                title="Cargabilidad"
+                subtitle="Calendario de consultores"
+                contentSubtitleComponent={
+                    <>
+                        <Calendario
+                            onclose={handleClose}
+                            workerId="1"
+                            ordenInterna=""
+                        />
+                    </>
+                }
+            >
+                <ContentTable
+                    header={<SearchWorkers />}
+                    Body={
+                        <Table_1
+                            headers={["Consultor", "Especialidad", "Departamento", "Esquema", "Tiempo", "Estatus", ""]}
+                            rows={staf.staff.map((p) => [p.consultor, p.especialidad, p.departamento, p.esquema, p.tiempo, p.estatus,
+                            <>
+                                <Btn_data text="..." Onclick={() => toggleMenu(p.id)} />
+                                {menuOpenId === p.id && (
 
-                )}
-                </>
-                ])}
-            />
-            }
-        />
+                                    <Btn_list
+                                        items={[
+                                            {
+                                                text: "Horas",
+                                                onClick: () => handleOption("horas", p.id),
+                                            },
+                                            {
+                                                text: "Proyectos",
+                                                onClick: () => handleOption("proyectos", p.id),
+                                            },
+                                        ]}
+                                    />
 
-        </ContentBody>
+                                )}
+                            </>
+                            ])}
+                        />
+                    }
+                />
 
-        {/* fin de el contenedor de tabla */}
+            </ContentBody>
 
-        {/* Modal*/}
-        <DialogBase
-        close={handleClose}
-        dialogRef={dialogRef}>
-        <LogicaSwtich
-            id={selectId}
-            n={selectedAction}
-        />
-        </DialogBase>
-    </>
+            {/* fin de el contenedor de tabla */}
+
+            {/* Modal*/}
+            <DialogBase
+                close={handleClose}
+                dialogRef={dialogRef}>
+                <LogicaSwtich
+                    id={selectId}
+                    n={selectedAction}
+                />
+            </DialogBase>
+        </>
     )
 
 
--- src/components/containers/containers.tsx
+++ src/components/containers/containers.tsx
@@ -1,11 +1,18 @@
 import React from "react"
 
 // contenedores
-export function ContentBody({ children, title }: { title?: string, children?: React.ReactNode }) {
+export function ContentBody({ children, title, subtitle, contentSubtitleComponent }: { title?: string, children?: React.ReactNode, subtitle?: string, contentSubtitleComponent?: React.ReactNode }) {
     const contentTitle = <h2 className="mb-8 text-2xl font-bold">{title}</h2>
+    const ContentSubtitle = <h2 className="mb-8 text-2xl font-bold">{subtitle}</h2>
     return (
         <div className="p-1">
             {contentTitle}
+            {subtitle && (
+                <div className="py-2 px-2 overflow-hidden rounded-lg border border-gray-300 bg-white p-0 shawdow-md dark:border-slate-700 dark:bg-slate-800 mb-2.5">
+                    {ContentSubtitle}
+                    {contentSubtitleComponent}
+                </div>
+            )}
             <div className="py-2 px-2 overflow-hidden rounded-lg border border-gray-300 bg-white p-0 shawdow-md dark:border-slate-700 dark:bg-slate-800">
                 {children}
             </div>
