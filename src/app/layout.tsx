54de09509c26637f1448cd0e6d2e2ab1460c7a5d
AbrahaHub <apple_abraham@hotmail.com>
2025-12-10 10:46:32 -0600

    chore: actualizar dependencias de Clerk y agregar localizaciones en español

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { esES } from "@clerk/localizations";
 import "./globals.css";
 import GlobalWrapper from "@/components/GlobalWrapper";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <ClerkProvider>
+        <ClerkProvider localization={esES}>
             <html lang="es" suppressHydrationWarning>
                 <body>
                     <GlobalWrapper>
